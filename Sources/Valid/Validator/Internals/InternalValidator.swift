27005db38183af21ab7343b69304f15274618d59
Mathias Quintero <me@quintero.io>
2022-01-19 00:37:37 +0000

    Adding basic validation context

--- Sources/Valid/Validator/Internals/InternalValidator.swift
+++ Sources/Valid/Validator/Internals/InternalValidator.swift
@@ -2,4 +2,8 @@
 import Foundation
 
 class InternalValidator<Input> {
+
+    func validate(input: Input, on context: ValidationContext<Input>) async -> ValidationResult {
+        return .skip
+    }
 }
---
+++ Sources/Valid/Validator/Internals/ValidationContext.swift
@@ -0,0 +1,38 @@
+
+import Foundation
+
+class ValidationContext<Input> {
+    private let value: Input
+    let lazy: Bool
+    private var diagnostics: Diagnostics
+    private var children: [BasicValidationResults<Input>.AnyChild]
+    private(set) var checks: [Check]
+
+    init(value: Input, lazy: Bool = true) {
+        self.value = value
+        self.lazy = lazy
+        self.diagnostics = Diagnostics(successes: [], warnings: [], errors: [])
+        self.children = []
+        self.checks = []
+    }
+
+    func check(_ check: Check) {
+        self.checks.append(check)
+    }
+
+    func success(_ diagnostic: Diagnostic) {
+        diagnostics.successes.append(diagnostic)
+    }
+
+    func error(_ diagnostic: Diagnostic) {
+        diagnostics.errors.append(diagnostic)
+    }
+
+    func warning(_ diagnostic: Diagnostic) {
+        diagnostics.warnings.append(diagnostic)
+    }
+
+    func validate(using validator: InternalValidator<Input>) async -> ValidationResult {
+        return await validator.validate(input: value, on: self)
+    }
+}
