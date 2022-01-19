5197bd65424c20d2cac3a16f0d7db62e819bd167
Mathias Quintero <me@quintero.io>
2022-01-19 00:35:32 +0000

    Adding internal validator API

--- Sources/Valid/Validator/ValidationRules.swift
+++ Sources/Valid/Validator/ValidationRules.swift
@@ -2,6 +2,11 @@
 import Foundation
 
 public class BaseValidationRules<Input> {
+    let validator: InternalValidator<Input>
+
+    init(validator: InternalValidator<Input>) {
+        self.validator = validator
+    }
 }
 
 public class PartialValidationRules<Input>: BaseValidationRules<Input> {
