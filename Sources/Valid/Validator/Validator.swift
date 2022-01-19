f7d44525b5d201c80e0da5194193d848a027c8e1
Mathias Quintero <me@quintero.io>
2022-01-19 00:40:04 +0000

    Adding api for getting restuls from validator

--- Sources/Valid/Validator/Validator.swift
+++ Sources/Valid/Validator/Validator.swift
@@ -7,3 +7,37 @@ public protocol Validator {
     @ValidationRulesBuilder
     var rules: ValidationRules<Input> { get }
 }
+
+extension Validator {
+
+    public func validate(input: Input, lazy: Bool = true) async -> Validation<Input> {
+        let context = ValidationContext(value: input, lazy: lazy)
+        let result = await context.validate(using: rules.validator)
+        let verdict: Validation<Input>.Verdict
+        switch result {
+        case .allow(let message):
+            verdict = .allow(message: message)
+        case .deny(let message):
+            verdict = .deny(message: message)
+        case .skip:
+            verdict = .deny(message: nil)
+        }
+        return Validation(verdict: verdict, checks: context.checks, results: context.build())
+    }
+
+    public func isValid(input: Input) async -> Bool {
+        let validation = await validate(input: input)
+        switch validation.verdict {
+        case .allow:
+            return true
+        case .deny:
+            return false
+        }
+    }
+
+    public func checks(input: Input, lazy: Bool) async -> [Check] {
+        let validation = await validate(input: input, lazy: lazy)
+        return validation.checks
+    }
+
+}
