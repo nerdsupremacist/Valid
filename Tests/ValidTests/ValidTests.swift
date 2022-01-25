c660e587e3b2a15dc34bcad63d482249f926dce5
Mathias Quintero <me@quintero.io>
2022-01-25 23:10:19 +0000

    Usign new rule in password test

--- Tests/ValidTests/ValidTests.swift
+++ Tests/ValidTests/ValidTests.swift
@@ -9,10 +9,14 @@ struct PasswordValidator: Validator {
                 return "Characters \(list) are not allowed"
             }
 
+        DenyIfContainsTooFewCharactersFromSet(.decimalDigits, minimum: 2, message: "Must contain at least 2 numbers")
+
+        DenyIfContainsTooFewCharactersFromSet(.capitalizedLetters, minimum: 1, message: "Must contain at least 1 capitalized letter")
+
         Property(\String.count) {
             DenyIf("Length must not be even") { $0 % 2 == 0 }
 
-            DenyIfTooSmall(minimum: 8)
+            DenyIfTooSmall(minimum: 8, message: "Must be at least 8 characters long")
         }
 
         AlwaysAllow<String>()
@@ -25,5 +29,6 @@ final class ValidTests: XCTestCase {
         let validator = PasswordValidator()
         let results = await validator.validate(input: "12😂345", lazy: false)
         print(results.all.errors)
+        print(results.checks)
     }
 }
