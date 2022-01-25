29aae0ca2c241af404cbfaaa951b515c0bfa749f
Mathias Quintero <me@quintero.io>
2022-01-25 23:09:59 +0000

    Cleaning up package file

--- Package.swift
+++ Package.swift
@@ -7,23 +7,15 @@ let package = Package(
     name: "Valid",
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Valid",
-            targets: ["Valid"]),
+        .library(name: "Valid",
+                 targets: ["Valid"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Valid",
-            dependencies: []),
-        .testTarget(
-            name: "ValidTests",
-            dependencies: ["Valid"]),
+        .target(name: "Valid",
+                dependencies: [],
+                exclude: ["Validator/ValidationRulesBuilder.swift.gyb"]),
+        .testTarget(name: "ValidTests",
+                    dependencies: ["Valid"]),
     ]
 )
