b3794c2605a357c94a7688454935f03b8739f67c
Mathias Quintero <me@quintero.io>
2022-02-06 21:45:17 +0000

    Removing messages from comparable calls

--- Sources/Valid/Validator/Rules/Implementations/Common/Comparable/DenyIfTooSmall.swift
+++ Sources/Valid/Validator/Rules/Implementations/Common/Comparable/DenyIfTooSmall.swift
@@ -2,40 +2,17 @@
 import Foundation
 
 public struct DenyIfTooSmall<Input: Comparable>: MaybeDenyValidationRule {
-    public typealias MessageBuilder = (Input, Input) -> String
+    public let minimum: Input
 
-    private let minimum: Input
-    private let message: MessageBuilder
-    
     public init(minimum: Input) {
         self.minimum = minimum
-        self.message = DenyIfTooSmall<Input>.standardMessage(input:minimum:)
-    }
-
-    public init(minimum: Input, message: String) {
-        self.minimum = minimum
-        self.message = { _, _ in message }
-    }
-
-    public init(minimum: Input, message: @escaping MessageBuilder) {
-        self.minimum = minimum
-        self.message = message
     }
 
     public func evaluate(on input: Input) async -> MaybeDeny {
         if input < minimum {
-            let message = self.message(input, minimum)
-            return .deny(message: message)
+            return .deny
         }
 
         return .skip
     }
-}
-
-extension DenyIfTooSmall {
-
-    private static func standardMessage(input: Input, minimum: Input) -> String {
-        return "Expected \(input) to be at least \(minimum)"
-    }
-
 }
