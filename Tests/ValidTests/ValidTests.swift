205347de7ec179d403fee4d0c8118f4d295bc72b
Mathias Quintero <me@quintero.io>
2022-02-06 21:46:38 +0000

    Updating tests

--- Tests/ValidTests/ValidTests.swift
+++ Tests/ValidTests/ValidTests.swift
@@ -1,28 +1,50 @@
 import XCTest
 import Valid
 
+extension Set where Element == String {
+    static let bannedPasswords: Set<String> = [
+        "password",
+        "123456"
+    ]
+}
+
+extension CharacterSet {
+    public static let allowedPasswordCharacters: CharacterSet = .letters.union(.decimalDigits).union(.punctuationCharacters)
+}
+
 struct PasswordValidator: Validator {
     var rules: ValidationRules<String> {
-        DenyIfContainsInvalidCharacters(allowedCharacters: .letters.union(.decimalDigits).union(.punctuationCharacters))
-            .withMessage { characters in
+        AllowIfEquals("Super secret")
+
+        DenyIfContainsInvalidCharacters(allowedCharacters: .allowedPasswordCharacters)
+            .message { characters in
                 let list = characters.map { "\"\($0)\"" }.joined(separator: ", ")
                 return "Characters \(list) are not allowed"
             }
 
-        DenyIfContainsTooFewCharactersFromSet(.decimalDigits, minimum: 2, message: "Must contain at least 2 numbers")
+        DenyIfContainsTooFewCharactersFromSet(.decimalDigits, minimum: 2)
+            .message("Must contain at least 2 numbers")
 
-        DenyIfContainsTooFewCharactersFromSet(.uppercaseLetters, minimum: 1, message: "Must contain at least 1 uppercase letter")
+        DenyIfContainsTooFewCharactersFromSet(.uppercaseLetters, minimum: 1)
+            .message("Must contain at least 1 uppercase letter")
+
+        DenyIfPartOfSet<String>(.bannedPasswords)
+            .message { _, rule in
+                return "Can't be part of the banned password list (\(rule.set.sorted().joined(separator: ", "))"
+            }
 
         Property(\String.count) {
             DenyIf("Length must not be even") { $0 % 2 == 0 }
 
-            DenyIfTooSmall(minimum: 8, message: "Must be at least 8 characters long")
+            DenyIfTooSmall(minimum: 8)
+                .message { count, rule in
+                    return "Must be at least \(rule.minimum) characters long (was \(count))"
+                }
         }
 
         AlwaysAllow<String>()
     }
 }
-
 
 final class ValidTests: XCTestCase {
     func testInvalidCharacters() async {
@@ -32,7 +54,7 @@ final class ValidTests: XCTestCase {
         let errors = results.all.errors
         XCTAssertEqual(errors.count, 1)
         XCTAssertEqual(errors[0].message, "Characters \"😂\" are not allowed")
-        XCTAssertEqual(errors[0].location.line, 6)
+        XCTAssertEqual(errors[0].location.line, 19)
     }
 
     func testTooFewDecimals() async {
@@ -42,7 +64,7 @@ final class ValidTests: XCTestCase {
         let errors = results.all.errors
         XCTAssertEqual(errors.count, 1)
         XCTAssertEqual(errors[0].message, "Must contain at least 2 numbers")
-        XCTAssertEqual(errors[0].location.line, 12)
+        XCTAssertEqual(errors[0].location.line, 25)
     }
 
     func testNoCapitals() async {
@@ -52,7 +74,7 @@ final class ValidTests: XCTestCase {
         let errors = results.all.errors
         XCTAssertEqual(errors.count, 1)
         XCTAssertEqual(errors[0].message, "Must contain at least 1 uppercase letter")
-        XCTAssertEqual(errors[0].location.line, 14)
+        XCTAssertEqual(errors[0].location.line, 28)
     }
 
     func testEven() async {
@@ -62,7 +84,7 @@ final class ValidTests: XCTestCase {
         let errors = results.all.errors
         XCTAssertEqual(errors.count, 1)
         XCTAssertEqual(errors[0].message, "Length must not be even")
-        XCTAssertEqual(errors[0].location.line, 17)
+        XCTAssertEqual(errors[0].location.line, 37)
     }
 
     func testTooSmall() async {
@@ -71,8 +93,8 @@ final class ValidTests: XCTestCase {
         XCTAssertDenies(results)
         let errors = results.all.errors
         XCTAssertEqual(errors.count, 1)
-        XCTAssertEqual(errors[0].message, "Must be at least 8 characters long")
-        XCTAssertEqual(errors[0].location.line, 19)
+        XCTAssertEqual(errors[0].message, "Must be at least 8 characters long (was 5)")
+        XCTAssertEqual(errors[0].location.line, 39)
     }
 
     func testValid() async {
